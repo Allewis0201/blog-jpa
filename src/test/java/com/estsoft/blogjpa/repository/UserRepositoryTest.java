e5b9c87d4dec583c427dacac28a2f6603f9e252a
allewis <allewis0201@gmail.com>
2024-03-22 11:10:46 +0900

    3/22 properties 파일 추가 및 UserRepositoryTest 코드 추가

---
+++ src/test/java/com/estsoft/blogjpa/repository/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.estsoft.blogjpa.repository;
+
+import com.estsoft.blogjpa.domain.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+    @Mock
+    UserRepository userRepository;
+
+    @Test
+    public void createTest()
+    {
+
+    }
+
+}
